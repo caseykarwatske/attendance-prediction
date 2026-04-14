ab0541052c278efc5041076a3aed9d8235955ca8
Casey Karwatske <caseykarwatske@gmail.com>
2026-04-14 10:53:19 -0400

    remove useless program

--- graph_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-data = pd.read_csv("data/dataset.csv")
-
-plt.plot(data.loc[data["Semester"] == 0, ["SemOffset"]], data.loc[data["Semester"] == 0, ["PastAttendance"]])
-plt.plot(data.loc[data["Semester"] == 1, ["SemOffset"]], data.loc[data["Semester"] == 1, ["PastAttendance"]])
-
-plt.show()
