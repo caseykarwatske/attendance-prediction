137822c221f9b29bd7577bc1e653b22837818bc3
Casey Karwatske <caseykarwatske@gmail.com>
2026-04-14 19:33:46 -0400

    most recent

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,12 +7,13 @@ from sklearn.metrics import r2_score
 
 attendance_data = pd.read_csv("data/dataset.csv")
 
-X = attendance_data[['Semester', 'SemOffset']]
+X = attendance_data[['SemOffset', 'Semester']]
 y = attendance_data['PastAttendance']
 
 # Reference for main model 
 model = LinearRegression()
 
+# Attempt to linearize data though y-transformations on the ladder of powers
 def generate_best_fit(lam):
     # Instantiate experimental model
     experiment = LinearRegression()
@@ -28,9 +30,6 @@ def generate_best_fit(lam):
         experiment.fit(X, np.log(y))
         r2 = r2_score(np.log(y), experiment.predict(X))
 
-    # Display r-squared
-    print(f'R-squared (lam = {lam}): {r2}')
-
     nxt = generate_best_fit(lam - 0.5) if lam != -1 else (0, 0)
 
     if r2 < nxt[1]:
@@ -45,16 +44,35 @@ print(f"Fitting model with transformation of lambda = {lam} for expected r-squar
 # Fit the model using the best transformation
 model.fit(X, y ** lam if lam != 0 else np.log(y))
 
-# Form results dataframe
-results = X.join(pd.DataFrame(model.predict(X))).set_axis(['Semester', 'SemOffset', 'AttendanceBar'], axis=1)
-results['AttendanceBar'] = results['AttendanceBar'] ** lam if lam != 0 else np.exp(results['AttendanceBar'])
+def generate_budget(attendance, total_budget):
+    # Integrate to find total attendance predicted for semester
+    total_attendance = pd.DataFrame(model.predict(X) ** lam if lam != 0 else np.exp(model.predict(X))).sum().item()
 
-# Plot fall semester values and predictions
-plt.scatter(attendance_data[attendance_data['Semester'] == 0]['SemOffset'], attendance_data[attendance_data['Semester'] == 0]['PastAttendance'])
-plt.plot(results[results['Semester'] == 0]['SemOffset'], results[results['Semester'] == 0]['AttendanceBar'])
+    # Output an event budget of the same proportion to total budget as attendance to total attendance
+    return (attendance / total_attendance) * total_budget
 
-# Plot spring semester values and predictionss
-plt.scatter(attendance_data[attendance_data['Semester'] == 1]['SemOffset'], attendance_data[attendance_data['Semester'] == 1]['PastAttendance'])
-plt.plot(results[results['Semester'] == 1]['SemOffset'], results[results['Semester'] == 1]['AttendanceBar'])
+def plot_preds():
+    # Form results dataframe
+    results = X.join(pd.DataFrame(model.predict(X))).set_axis(['SemOffset', 'Semester', 'AttendanceBar'], axis=1)
+    results['AttendanceBar'] = results['AttendanceBar'] ** lam if lam != 0 else np.exp(results['AttendanceBar'])
 
-plt.show()
+    # Plot fall semester values and predictions
+    plt.scatter(attendance_data[attendance_data['Semester'] == 0]['SemOffset'], attendance_data[attendance_data['Semester'] == 0]['PastAttendance'])
+    plt.plot(results[results['Semester'] == 0]['SemOffset'], results[results['Semester'] == 0]['AttendanceBar'])
+
+    # Plot spring semester values and predictionss
+    plt.scatter(attendance_data[attendance_data['Semester'] == 1]['SemOffset'], attendance_data[attendance_data['Semester'] == 1]['PastAttendance'])
+    plt.plot(results[results['Semester'] == 1]['SemOffset'], results[results['Semester'] == 1]['AttendanceBar'])
+
+    plt.show()
+
+if len(sys.argv) == 1:
+    plot_preds()
+else:
+    event_features = pd.DataFrame({'SemOffset': [sys.argv[1]], 'Semester': [sys.argv[2]]})
+    event_pred = model.predict(event_features) ** lam if lam != 0 else np.exp(model.predict(event_features))
+
+    recommended_budget = generate_budget(event_pred.item(), int(sys.argv[3]))
+
+    print(f"Predicted Attendence: {event_pred.item() + 1}")
+    print(f"Recommended Budget: ${recommended_budget:.2f}")
