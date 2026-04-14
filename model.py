86ba12c806eda029dfc3f37e4422d374f4766269
Casey Karwatske <caseykarwatske@gmail.com>
2026-04-14 10:53:51 -0400

    remove unused imports

--- model.py
+++ model.py
@@ -1,9 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import r2_score
 
 attendance_data = pd.read_csv("data/dataset.csv")
